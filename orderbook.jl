cc44997b78ec6e689e7b8ffaccc05cfbe1e2f485
Gerardo Salazar <gsalaz9800@gmail.com>
2018-11-18 20:07:40 -0700

    Improve performance on orderbook creation

--- orderbook.jl
+++ orderbook.jl
@@ -1,11 +1,8 @@
+import Base.isless
 import Dates
 
 import CSV
-import Pandas
-import PyCall
-
-@PyCall.pyimport pandas
-@PyCall.pyimport numpy
+import DataFrames
 
 mutable struct Limit
     TimeStamp::Dates.Date
@@ -16,9 +13,21 @@ mutable struct Limit
     NextLimit::Union{Limit, Nothing}
 end 
 
-function create_orderbook(df::DataFrames.DataFrame, cb::Union{Function, Nothing}=nothing,
+mutable struct Orderbook
+    bids::Array{Limit, 1}
+    asks::Array{Limit, 1}
+end
+
+@inline function isless(itm_1::Limit, itm_2::Limit)::Bool
+    # Function is required to sort Limit items
+    return itm_1.price < itm_2.price
+end
+
+function create_simple_orderbook(df::DataFrames.DataFrame, current_orderbook::Union{Orderbook, Nothing}=nothing, cb::Union{Function, Nothing}=nothing,
                           mut_vector::Union{Array{Any}, Nothing}=nothing, pass_order::Bool=true)::Orderbook
-    # Takes in a dataframe with orderbook information.
+    # Takes orderbook deltas and converts it into a simple orderbook. Orders are not kept in a queue to keep track
+    # of order priority. That will come in the future. TODO
+    
     # Orderbook information is contained as book deltas with the following headers:
     #     :ts, :seq, :is_trade, :is_bid, :price, :volume
     #
@@ -38,126 +47,54 @@ function create_orderbook(df::DataFrames.DataFrame, cb::Union{Function, Nothing}
     #           A side effect of setting this value to `true` is that the `mut_vector` will contain
     #           a timestamp prepended to each value pushed to `mut_vector` as a matrix.
     
-    bids = Limit[Limit(Dates.unix2datetime(0), 0.0, 0.0)]
-    asks = Limit[Limit(Dates.unix2datetime(0), 0.0, 0.0)]
+    bids = Dict{Float64, Limit}()
+    asks = Dict{Float64, Limit}()
+    
+    if current_orderbook != nothing
+        bids = Dict{Float64, Limit}(zip([delta.price for delta in current_orderbook.bids], current_orderbook.bids))
+        asks = Dict{Float64, Limit}(zip([delta.price for delta in current_orderbook.asks], current_orderbook.asks))
+    end
+    
+    for delta in DataFrames.eachrow(df[df[:is_trade] .== false, :])
+        # Set to nothing so that we can select a side and not have repeating code
+        side = nothing
         
-    for order in DataFrames.eachrow(df[df[:is_trade] .== false, :])
-        # Determine where to insert the order in the bids/asks vector
-        if order[:is_bid]
-            # If we have no elements, we will not be able to reach any
-            # of the logic in the upcoming for loop. Fix it by inserting
-            # a dummy value into the vector       
-            for (index, level) in enumerate(bids)
-                if level.price == order[:price]
-                    if order[:volume] == 0.0
-                        # Delete the level if we have a zero sized order
-                        deleteat!(bids, index)
-                        
-                        # Run callback if we have one
-                        if cb != nothing && mut_vector != nothing && length(bids) > 1 && length(asks) > 1
-                            if pass_order
-                                cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, order, false)
-                            else
-                                cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, nothing, false)
-                            end
-                        end
-                        
-                        break
-                    end
-                    # Update the level if we have a duplicate price level
-                    bids[index] = Limit(order[:ts], order[:price], order[:volume])
-                    
-                    # Run callback if we have one
-                    if cb != nothing && mut_vector != nothing && length(bids) > 1 && length(asks) > 1
-                        if pass_order
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, order, false)
-                        else
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, nothing, false)
-                        end
-                    end
-                    
-                    break
-                elseif order[:price] > level.price
-                    # Don't add if the size is 0
-                    if order[:volume] == 0.0
-                        break
-                    end
-                    # For missing orders
-                    # Insert one level above encountered price
-                    insert!(bids, index, Limit(order[:ts], order[:price], order[:volume]))
-                    
-                    # Run callback if we have one
-                    if cb != nothing && mut_vector != nothing && length(bids) > 1 && length(asks) > 1
-                        if pass_order
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, order, false)
-                        else
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, nothing, false)
-                        end
-                    end
-                    
-                    break
-                end
-            end
+        if delta[:is_bid]
+            side = bids
         else
-            # If we have no elements, we will not be able to reach any
-            # of the logic in the upcoming for loop. Fix it by inserting
-            # a first value into the vector
-            for (index, level) in enumerate(asks)
-                if level.price == order[:price]
-                    if order[:volume] == 0.0
-                        # Delete the level if we have a zero sized order
-                        deleteat!(asks, index)
-                        
-                        # Run callback if we have one
-                        if cb != nothing && mut_vector != nothing && length(bids) > 1 && length(asks) > 1
-                            if pass_order
-                                cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, order, false)
-                            else
-                                cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, nothing, false)
-                            end
-                        end
-                        
-                        break
-                    end
-                    # Update the level if we have a duplicate price level
-                    asks[index] = Limit(order[:ts], order[:price], order[:volume])
-                    
-                    # Run callback if we have one
-                    if cb != nothing && mut_vector != nothing && length(bids) > 1 && length(asks) > 1
-                        if pass_order
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, order, false)
-                        else
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, nothing, false)
-                        end
-                    end
-                    
-                    break
-                    
-                elseif order[:price] > level.price
-                    # Don't add if the size is 0
-                    if order[:volume] == 0.0
-                        break
-                    end
-                    # For missing orders
-                    # Insert one level above encountered price
-                    insert!(asks, index, Limit(order[:ts], order[:price], order[:volume]))
-                    
-                    # Run callback if we have one
-                    if cb != nothing && mut_vector != nothing && length(bids) > 1 && length(asks) > 1
-                        if pass_order
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, order, false)
-                        else
-                            cb(Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)]), mut_vector, nothing, false)
-                        end
-                    end
-                    
-                    break
-                end
-            end
+            side = asks
+        end
+        
+        if delta[:volume] == 0.0
+            # Delete entry if size == 0.00
+            delete!(side, delta[:price])
+        else
+            # Update the price level to a new price
+            side[delta[:price]] = Limit(delta[:ts], delta[:price], delta[:volume])
+        end
+        
+        if cb != nothing && mut_vector != nothing && length(bids) > 0 && length(asks) > 0
+            # Reverse bids to have the first index be the best bid.
+            # We sort the orderbook as well so that we can compute depth appropriately, and have
+            # levels in sequential order.
+            
+            # We optimized this section of code to use as few allocations as possible.
+            # For maximum performance, ensure your callback is inlined.
+            cb(Orderbook(
+                    reverse(sort(collect(values(bids)))),
+                    sort(collect(values(asks)))),
+                
+                mut_vector, 
+                pass_order ? delta : nothing, 
+                false)
         end
     end
     
-    return Orderbook(bids[1:length(bids)-1], reverse(asks)[2:length(asks)])
+    # Return sorted orderbook
+    return Orderbook(
+        reverse(sort(collect(values(bids)))), 
+        sort(collect(values(asks))))
+    
 end
 
 @inline function mid_price(book::Orderbook, mut_vector::Union{Array{Any}, Nothing}, 
